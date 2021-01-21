65f9ca96ffd7eed88429faf9ac2d8fbac6ead967
Piotrek Zawal <piotr.zawal@gmail.com>
2021-01-21 21:27:46 +0100

    Update m7_expense_tracker.sql
    corrected solutions

--- m7_expense_tracker.sql
+++ m7_expense_tracker.sql
@@ -21,9 +21,12 @@
 	         u.user_login
         FROM expense_tracker.bank_account_owner bao
   INNER JOIN expense_tracker.bank_account_types bat ON bao.id_ba_own = bat.id_ba_own
+                                                   AND bat.actibe = '1'
   INNER JOIN expense_tracker.transaction_bank_accounts tba ON bat.id_ba_own = tba.id_ba_own
   INNER JOIN expense_tracker.users u ON tba.id_ba_own = u.id_user
-       WHERE owner_name = 'Janusz Kowalski' AND bat.active = '1';
+       WHERE owner_name = 'Janusz Kowalski'; -- AND bat.active = '1';
+       -- warunki w where lepiej wpleść w JOINY - lepsza wydajność im wcześniej zaczniemy filtrować rekordy
+       -- są powielone rekordy bo brakuje warunku 'AND tba.id_ba_typ = bat.id_ba_type' 
  
 
 -- 2. Wyświetl wszystkie informacje o dostępnych kategoriach transakcji i ich możliwych
@@ -41,16 +44,18 @@ LEFT JOIN expense_tracker.transaction_subcategory ts ON tc.id_trans_cat = ts.id_
     WHERE tc.active IN ('1', 'TRUE', 'Y', 'y')
       AND ts.active IN ('1', 'TRUE', 'Y', 'y')
  ORDER BY tc.id_trans_cat;
-
+-- trochę prościej: można CASTować tc.active CAST tc.active AS BOOLEAN/ CAST tc.active IS TRUE
+-- albo przed INSERTem: sprawdzić czy tc.active jest danego typu
 
 -- 3. Wyświetl wszystkie transakcje (TRANSACTIONS), które miały miejsce w 2016 roku
 --    związane z kategorią JEDZENIE.
 
        SELECT *
          FROM expense_tracker.transactions t
-    LEFT JOIN expense_tracker.transaction_category tc ON t.id_trans_cat = tc.id_trans_cat 
+         JOIN expense_tracker.transaction_category tc ON t.id_trans_cat = tc.id_trans_cat 
         WHERE EXTRACT(YEAR FROM t.transaction_date) = 2016 
-	     AND category_name = 'JEDZENIE';
+	      AND category_name = 'JEDZENIE';
+
 
 -- 4. Dodaj nową podkategorię do tabeli TRANSACTION_SUBCATEGORY, która będzie w relacji
 --    z kategorią (TRANSACTION_CATEGORY) JEDZENIE.
@@ -64,6 +69,7 @@ INSERT INTO expense_tracker.transaction_subcategory (id_trans_cat, subcategory_n
      SELECT expense_tracker.transaction_category.id_trans_cat, 'Nowa podkategoria', 'Nowa podkategoria'
        FROM expense_tracker.transaction_category
       WHERE category_name = 'JEDZENIE';
+
      
 
 
@@ -82,10 +88,11 @@ SET id_trans_subcat = (
                       SELECT id_trans_subcat 
  					    FROM expense_tracker.transaction_subcategory
  					   WHERE subcategory_name = 'Nowa podkategoria'
+                            LIMIT 1 -- zapobiega błędowi jeśli jest więcej niż 1 rekord
  					   )
 WHERE EXISTS
      (
-     SELECT *
+     SELECT 1
        FROM no_subcat
       WHERE no_subcat.id_trans_subcat = ts.id_trans_subcat
      );
@@ -100,6 +107,7 @@ UPDATE expense_tracker.transactions tr
  					     )
  WHERE id_trans_subcat = -1;
 -- https://stackoverflow.com/questions/36908495/update-with-result-from-cte-postgresql
+-- co jeśli więcej informacji ma wartość -1? wówczas wszystkie się zaktualizują; warto przemyśleć ograniczenie podzbioru
 
    
 -- 5. Wyświetl wszystkie transakcje w roku 2020 dla konta oszczędnościowego Janusz i
@@ -121,13 +129,38 @@ UPDATE expense_tracker.transactions tr
 	      AND bank_account_name LIKE '%OSZCZ%'
 	   )
     SELECT t.id_transaction,
-    	   tc.category_name,
-    	   ts.subcategory_name,
+    	      tc.category_name,
+    	      ts.subcategory_name,
+    	      tt.transaction_type_name,
+    	      t.transaction_date,
+    	      t.transaction_value 
+      FROM expense_tracker.transactions t 
+INNER JOIN ror_JG ON t.id_trans_ba = ror_JG.id_trans_ba
+                 AND EXTRACT(YEAR FROM transaction_date) = 2020
+ LEFT JOIN expense_tracker.transaction_type tt ON t.id_trans_type = tt.id_trans_type
+ LEFT JOIN expense_tracker.transaction_category tc ON t.id_trans_cat = tc.id_trans_cat
+ LEFT JOIN expense_tracker.transaction_subcategory ts ON t.id_trans_subcat = ts.id_trans_subcat;
+
+
+-- brakuje warunku na rok 2020
+-- spróbuj przepisać na wersję bez CTE
+-- poprawione:
+
+    SELECT tc.category_name,
+       	   ts.subcategory_name,
     	   tt.transaction_type_name,
     	   t.transaction_date,
     	   t.transaction_value 
-      FROM expense_tracker.transactions t 
-INNER JOIN ror_JG ON t.id_trans_ba = ror_JG.id_trans_ba
+      FROM expense_tracker.transactions t
+INNER JOIN 
+          (
+             SELECT *
+               FROM expense_tracker.bank_account_owner bao
+          LEFT JOIN expense_tracker.transaction_bank_accounts tba ON bao.id_ba_own = tba.id_ba_own
+              WHERE owner_name = 'Janusz i Grażynka'
+                AND bank_account_name LIKE '%OSZCZ%'
+     ) ror_JG ON t.id_trans_ba = ror_JG.id_trans_ba
+             AND EXTRACT(YEAR FROM t.transaction_date) = 2020
  LEFT JOIN expense_tracker.transaction_type tt ON t.id_trans_type = tt.id_trans_type
  LEFT JOIN expense_tracker.transaction_category tc ON t.id_trans_cat = tc.id_trans_cat
  LEFT JOIN expense_tracker.transaction_subcategory ts ON t.id_trans_subcat = ts.id_trans_subcat;
