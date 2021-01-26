1e8bf33ab5845be3dc012cbba8f6b0ae1d745597
Piotrek Zawal <piotr.zawal@gmail.com>
2021-01-26 22:10:01 +0100

    added module 8 exercises

---
+++ m8_expense_tracker.sql
@@ -0,0 +1,13 @@
+-- 1. Oblicz sumę transakcji w podziale na kategorie transakcji. W wyniku wyświetl nazwę
+--    kategorii i całkowitą sumę.
+-- 2. Oblicz sumę wydatków na Używki dokonana przez Janusza (Janusz Kowalski) z jego
+--    konta prywatnego (ROR - Janusz) w obecnym roku 2020.
+-- 3. Stwórz zapytanie, które będzie podsumowywać wydatki (typ transakcji: Obciążenie) na
+--    wspólnym koncie RoR - Janusza i Grażynki w taki sposób, aby widoczny był podział
+--    sumy wydatków, ze względu na rok, rok i kwartał (format: 2019_1), rok i miesiąc (format:
+--    2019_12) w roku 2019. Skorzystaj z funkcji ROLLUP.
+-- 4. Stwórz zapytanie podsumowujące sumę wydatków na koncie wspólnym Janusza i
+--    Grażynki (ROR- Wspólny), wydatki (typ: Obciążenie), w podziale na poszczególne lata
+--    od roku 2015 wzwyż. Do wyników (rok, suma wydatków) dodaj korzystając z funkcji
+--    okna atrybut, który będzie różnicą pomiędzy danym rokiem a poprzednim (balans rok
+--    do roku).
