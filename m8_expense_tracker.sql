02d1908f983eeb235159a616c462ae768bfe4d62
Piotr Zawal <piotr.zawal@gmail.com>
2021-02-18 16:42:42 +0100

    corrected last_view() function in ex5

--- m8_expense_tracker.sql
+++ m8_expense_tracker.sql
@@ -33,6 +33,22 @@ SELECT *
 SELECT sum(ue.transaction_value) sum_ROR_exp_2020
   FROM uzywki_exp ue
   JOIN janusz_ror ON janusz_ror.id_ba_type = ue.id_trans_ba;
+ 
+-- w tym wypadku lepiej nie używać CTE, bo zaciemniają obraz:
+
+SELECT sum(t.transaction_value)
+  FROM expense_tracker.transactions t
+  JOIN expense_tracker.transaction_category tc ON tc.id_trans_cat = t.id_trans_cat
+                                              AND tc.category_name = 'UŻYWKI'
+  JOIN expense_tracker.transaction_bank_accounts tba ON tba.id_trans_ba = t.id_trans_ba
+  JOIN expense_tracker.bank_account_owner bao ON bao.id_ba_own = tba.id_ba_own
+  JOIN expense_tracker.bank_account_types bat ON bat.id_ba_type = tba.id_ba_typ
+										     AND bat.id_ba_own = bao.id_ba_own 
+										     AND bat.ba_type = 'ROR'
+  JOIN expense_tracker.users u ON u.id_user = bao.user_login 
+	 						  AND u.user_login = 'jkowalski'
+ WHERE EXTRACT(YEAR FROM t.transaction_date) = 2020;
+
 
 -- 3. Stwórz zapytanie, które będzie podsumowywać wydatki (typ transakcji: Obciążenie) na
 --    wspólnym koncie RoR - Janusza i Grażynki w taki sposób, aby widoczny był podział
@@ -59,7 +75,7 @@ LEFT JOIN expense_tracker.transaction_type tt ON tt.id_trans_type = t.id_trans_t
       GROUP BY ROLLUP (yr, 
                        yr_qtr,
                        yr_mnth)
-                      ORDER BY (2, 3);
+ ORDER BY yr_qtr, yr_mnth;
 
 -- 4. Stwórz zapytanie podsumowujące sumę wydatków na koncie wspólnym Janusza i
 --    Grażynki (ROR- Wspólny), wydatki (typ: Obciążenie), w podziale na poszczególne lata
@@ -105,25 +121,48 @@ SELECT EXTRACT (YEAR FROM t.transaction_date) transaction_year,
            WHERE EXTRACT (YEAR FROM t.transaction_date) >= 2015
 GROUP BY transaction_year;
 
+-- poprawiona wersja:
 
+WITH common_spendings AS (
+	SELECT EXTRACT(YEAR FROM t.transaction_date) AS transaction_year,
+	       sum(t.transaction_value) AS yearly_transaction_value
+	  FROM expense_tracker.transactions t
+	  JOIN expense_tracker.transaction_category tc ON tc.id_trans_cat = t.id_trans_cat 
+	  JOIN expense_tracker.transaction_bank_accounts tba ON tba.id_trans_ba = t.id_trans_ba
+	  JOIN expense_tracker.bank_account_owner bao ON bao.id_ba_own = tba.id_ba_own 
+	  JOIN expense_tracker.bank_account_types bat ON bat.id_ba_type = tba.id_ba_typ 
+	  											 AND bat.id_ba_own = bao.id_ba_own 
+	  											 AND bat.ba_type = 'ROR - WSPÓLNY'
+	  JOIN expense_tracker.transaction_type tt ON tt.id_trans_type = t.id_trans_type
+	                                          AND tt.transaction_type_name = 'Obciążenie'
+     WHERE EXTRACT(YEAR FROM t.transaction_date) >= 2015
+     GROUP BY transaction_year
+) SELECT *,
+         (yearly_transaction_value - prev_year_spend)*(-1) AS increase_of_spend_yoy
+    FROM (
+    	  SELECT *,
+    		     lag(yearly_transaction_value) OVER (ORDER BY transaction_year) AS prev_year_spend
+    	    FROM common_spendings 
+    	 ) sq;
+  
 
 -- 5. Korzystając z funkcji LAST_VALUE pokaż różnicę w dniach, pomiędzy kolejnymi
 --    transakcjami (Obciążenie) na prywatnym koncie Janusza (RoR) dla podkategorii
 --    Technologie w 1 kwartale roku 2020.
 
 SELECT t.id_transaction,
-	 t.transaction_date,
-	 t.transaction_date  - last_value(t.transaction_date) 
+	 t.transaction_date,	
+	 t.transaction_date - last_value(t.transaction_date) 
 	 					   		OVER (ORDER BY t.transaction_date ASC
 	 			                      GROUPS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS days_from_prev_purchase
   FROM expense_tracker.transactions t
   JOIN expense_tracker.transaction_bank_accounts tba ON tba.id_trans_ba = t.id_trans_ba
+  													AND tba.bank_account_name = 'ROR - Janusz'
   JOIN expense_tracker.transaction_type tt ON tt.id_trans_type = t.id_trans_type
   JOIN expense_tracker.transaction_subcategory ts ON ts.id_trans_subcat = t.id_trans_subcat
- WHERE tba.bank_account_name = 'ROR - Janusz'
- 	-- AND t.transaction_date BETWEEN '2020-01-01' AND '2020-03-31' - w tym kwartale mam tylko 1 rekord, dlatego wziąłem dane dla całego zakresu
- 	AND ts.subcategory_description = 'Technologie'
- 	
+  												 AND ts.subcategory_description = 'Technologie';
+ 	--WHERE t.transaction_date BETWEEN '2020-01-01' AND '2020-03-31' - w tym kwartale mam tylko 1 rekord, dlatego wziąłem dane dla całego zakresu
+
 -- mam wrażenie, że coś przekombinowałem w tym rozwiązaniu :)
 -- czy za pomocą last_value() da się zrobić tak, aby dla kolejnych transakcji wykonanych w danym dniu
 -- pokazywał 0 (np. obecnie dla 2015-12-19 mam 4 transakcje i każda z nich pokazuje 11 dni od poprzedniej.
